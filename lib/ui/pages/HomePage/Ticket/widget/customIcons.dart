076f17072f291c91811d8fd473ac298634b88c30
ldiazg97Klint <lester@klint.cl>
2025-03-12 02:33:47 -0400

    Mejora el widget CustomSeatIcon al agregar el número de asiento y ajustar los íconos según el estado de selección y ocupación

--- lib/ui/pages/HomePage/Ticket/widget/customIcons.dart
+++ lib/ui/pages/HomePage/Ticket/widget/customIcons.dart
@@ -1,36 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:BusGo/models/SeatModel.dart';
 
 class CustomSeatIcon extends StatelessWidget {
   final bool isOccupied;
   final bool isSelected;
+  final int seatNumber; // Número del asiento
 
-  CustomSeatIcon({required this.isOccupied, required this.isSelected});
+  const CustomSeatIcon({
+    super.key,
+    required this.isOccupied,
+    required this.isSelected,
+    required this.seatNumber,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       width: 40, // Ancho ajustado
       height: 40, // Alto ajustado
       child: Stack(
         alignment: Alignment.center,
         children: [
           Icon(
-            Icons.event_seat,
-            size: 30,
+            isSelected
+                ? Icons
+                    .crop_square_rounded // Nuevo ícono cuando isSelected es true
+                : Icons.square_rounded, // Ícono predeterminado
+            size: 65,
             color: isOccupied
-                ? Colors.red // Si está ocupado, color rojo
+                ? Colors.black54 // Si está ocupado, color rojo
                 : isSelected
                     ? Colors
-                        .blue // Si está seleccionado por el usuario, color azul
-                    : Colors.green, // Si está disponible, color verde
+                        .black54 // Color del borde cuando isSelected es true
+                    : Colors.blue,
           ),
-          if (isOccupied || isSelected) // Si está ocupado o seleccionado
+          if (isSelected) // Si está ocupado o seleccionado
             Positioned(
-              top: 0,
-              child: Icon(
-                Icons.person,
-                size: 15, // Tamaño ajustado para evitar desbordamiento
-                color: Colors.blueGrey,
+              top: 19,
+              left: 19,
+              child: Text(
+                seatNumber.toString(),
+                style: const TextStyle(
+                  fontSize: 15,
+                  color: Colors.blueGrey,
+                  fontWeight: FontWeight.bold,
+                ), // Número del asiento
               ),
             ),
         ],
