fb97e24f5f543481d56e48b2838f6636901d7a61
lester <lester@klint.cl>
2025-04-23 01:23:18 -0400

    Add a signal to track the selected destination in sales views.
    - Introduce `selectedDestinationSignal` to track the currently selected destination.
    - The signal is initialized with `null`, indicating that all destinations should be shown.

--- lib/ui/pages/HomePage/Sales/SalesPage.dart
+++ lib/ui/pages/HomePage/Sales/SalesPage.dart
@@ -4,16 +4,22 @@ import 'package:BusGo/ui/pages/HomePage/Sales/widget/ScheduleCardWidget.dart';
 import 'package:flutter/material.dart';
 import 'package:signals/signals_flutter.dart';
 
+import '../../../../domain/signals/login_signals/login_signal.dart';
+import '../../../../domain/signals/sales_signals/sales_signals.dart';
+
 class SalesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final branchName = currentUserBranchLG.value?.name ?? "Sucursal no asignada";
     return Scaffold(
       backgroundColor: Colors.blue[400],
       body: SafeArea(
         child: Column(
           children: [
             // Header con imagen y título
-            const AppBarSalesWidget(),
+             AppBarSalesWidget(
+              origen: branchName,
+            ),
             // Card para detalles "From" y "To"
             Expanded(
               // Añadimos Expanded para que ocupe el espacio restante
@@ -53,39 +59,67 @@ class SalesPage extends StatelessWidget {
                                     ),
                                   ),
                                 )
-                              : ListView.builder(
-                                  shrinkWrap: true,
-                                  physics: const BouncingScrollPhysics(),
-                                  itemCount: tripsSignal.watch(context)!.length,
-                                  itemBuilder: (context, index) {
-                                    final trip =
-                                        tripsSignal.watch(context)![index];
-                                    availableSeatsSignal.value = trip.seats! -
-                                        (trip.reservedSeats!.length);
-                                    return ScheduleCard(
-                                      name: trip.name ?? '',
-                                      origin: trip.origin ?? "Desconocido",
-                                      destination:
-                                          trip.destination ?? "Desconocido",
-                                      plate: trip.plate ?? '',
-                                      originImage: trip.originImage ?? '',
-                                      destinationImage:
-                                          trip.destinationImage ?? '',
-                                      timeIni: trip.schedule.toString(),
-                                      timeFin: trip.arrival.toString(),
-                                      price: trip.price == null
-                                          ? trip.price.toString()
-                                          : "0.0",
-                                      place: trip.name ?? "Desconocido",
-                                      amount: trip.seats ?? 0,
-                                      seats: trip.seats ?? 0,
-                                      seatsAvailable: availableSeatsSignal
-                                          .value, //-ojo-verificar si fuera null que no de error
-                                      idTrip: trip.id ?? 0,
-                                      reservedSeats: trip.reservedSeats ?? [],
-                                    );
-                                  },
+                              : Watch((_) {
+                            final trips = tripsSignal.value ?? [];
+                            final selectedDestination = selectedDestinationSignal.value;
+
+
+                            final filteredTrips = selectedDestination != null
+                                ? trips.where((trip) => trip.destination == selectedDestination).toList()
+                                : trips;
+
+                            if (filteredTrips.isEmpty) {
+                              return const Padding(
+                                padding: EdgeInsets.only(top: 50),
+                                child: Center(
+                                  child: Text(
+                                    "No hay viajes disponibles",
+                                    style: TextStyle(color: Colors.grey, fontSize: 16),
+                                  ),
                                 ),
+                              );
+                            }
+
+
+                            return
+                            ListView.builder(
+                              shrinkWrap: true,
+                                physics: const NeverScrollableScrollPhysics(),
+                              itemCount: filteredTrips.length,
+                              itemBuilder: (context, index) {
+
+                                final trip = filteredTrips[index];
+                                final availableSeats = trip.seats! - (trip.reservedSeats?.length ?? 0);
+                                availableSeatsSignal.value = trip.seats! -
+                                    (trip.reservedSeats!.length);
+                                return ScheduleCard(
+                                  name: trip.name ?? '',
+                                  origin: trip.origin ?? "Desconocido",
+                                  destination:
+                                  trip.destination ?? "Desconocido",
+                                  plate: trip.plate ?? '',
+                                  originImage: trip.originImage ?? '',
+                                  destinationImage:
+                                  trip.destinationImage ?? '',
+                                  timeIni: trip.schedule.toString(),
+                                  timeFin: trip.arrival.toString(),
+                                  price: trip.price == null
+                                      ? trip.price.toString()
+                                      : "0.0",
+                                  place: trip.name ?? "Desconocido",
+                                  amount: trip.seats ?? 0,
+                                  seats: trip.seats ?? 0,
+                                  seatsAvailable: availableSeats,
+                                      // .value,
+                                  //-ojo-verificar si fuera null que no de error
+                                  idTrip: trip.id ?? 0,
+                                  reservedSeats: trip.reservedSeats ?? [],
+                                );
+                              },
+                            );
+
+                          }
+                              ),
                         ],
                       ),
                     ),
